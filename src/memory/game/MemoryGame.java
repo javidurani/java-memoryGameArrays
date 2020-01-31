03609f01bcd4e60099a12ad3aa9fd4f2fac2c521
Javier Duran <jduraniglesias21122@columbushs.com>
2020-01-31 09:34:16 -0500

    Working

--- src/memory/game/MemoryGame.java
+++ src/memory/game/MemoryGame.java
@@ -4,18 +4,37 @@
  * and open the template in the editor.
  */
 package memory.game;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
  * @author mlarrubia
  */
 public class MemoryGame {
-
+public static Scanner kb = new Scanner(System.in);
+public static Random random = new Random();
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        char[] deck={'A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H'};
+        boolean[] cardDrawn = new boolean[16];
+        int gridsize = 4;
+        char[][] grid = new char[4][4];
+        for(int i = 0; i < 16; i++){
+        cardDrawn[i] = false;
+        }
+        int randCard;
+        for(int j = 0; j < gridsize; j++){
+            for(int k = 0; k < gridsize; k++){
+                randCard = random.nextInt(16);
+                while(cardDrawn[randCard] == false){
+                    deck[randCard] = ' ';
+                }
+                
+            }
+        }        
     }
     
 }
